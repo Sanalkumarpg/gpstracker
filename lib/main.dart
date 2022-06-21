ebc57a6b17d2e1f687bc6241c047c201d5321449
sanalkumar <sanalkumar@triophore.com>
2022-06-21 23:22:00 +0530

    navigate from login to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import './login.dart';
+import './homepage.dart';
 
-  void main() {
-  runApp(
+void main() => runApp(const MyApp());
 
-      const MyApp());
+class MyApp extends StatelessWidget {
 
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Sample App';
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      title: 'triotrack',
+      //theme: ThemeData(
+       // primaryColor: kPrimaryColor,
+        //scaffoldBackgroundColor: Colors.white,
+        //home: Scaffold(
+          //appBar: AppBar(title: const Text(_title)),
+          //body: const MyStatefulWidget(),
+        //),
+      //),
+      home: MyStatefulWidget(),
+      routes: {
+        "/homepage": (_) =>  HomePage(),
+        "/signup" : (_) =>  MyStatefulWidget(),
+      },
+    );
   }
+}
 
 
 
